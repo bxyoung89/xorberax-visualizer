01b88978193d92735a599bd432a0289f748f8f42
Broderick Young <bxyoung89@gmail.com>
2018-05-02 22:30:17 -0500

    Changed format of image to be easier to render.

--- tools/gif2luminosity.js
+++ tools/gif2luminosity.js
@@ -4,15 +4,16 @@ const gifFrames = require('gif-frames');
 const gifInfo = require('gif-info');
 const toArrayBuffer = require('to-array-buffer');
 const jimp = require('jimp');
+const binarySearch = require('binary-search');
 
 const src = path.join(__dirname, process.argv[2]);
 const outDir = path.join(__dirname, process.argv[3]);
 
-gifFrames({url: src, frames: 'all', cumulative: true,}).then(
+gifFrames({url: src, frames: 'all', cumulative: true, quality: 100}).then(
 	async(frameData) => {
 		frameData.forEach(function (frame) {
 			frame.getImage().pipe(fs.createWriteStream(
-				path.join(outDir, `${frame.frameIndex}.png`)
+				path.join(outDir, `${frame.frameIndex}.jpg`)
 			));
 		});
 		const gifFile = fs.readFileSync(src);
@@ -27,10 +28,9 @@ gifFrames({url: src, frames: 'all', cumulative: true,}).then(
 			files: {},
 		};
 		await images.forEach(async(image, index) => {
-			const jimpFile = await jimp.read(path.join(outDir, `${index}.png`));
-			const fileLuminosity = [];
+			const jimpFile = await jimp.read(path.join(outDir, `${index}.jpg`));
+			const pixels = [];
 			for (let x = 0; x < jimpFile.bitmap.width; x += 1) {
-				const rowArray = [];
 				for (let y = 0; y < jimpFile.bitmap.height; y += 1) {
 					const pixelIndex = jimpFile.getPixelIndex(x, y);
 					const red = jimpFile.bitmap.data[pixelIndex];
@@ -38,16 +38,49 @@ gifFrames({url: src, frames: 'all', cumulative: true,}).then(
 					const blue = jimpFile.bitmap.data[pixelIndex + 2];
 
 					const rawLuminosity = (0.299 * red + 0.587 * green + 0.114 * blue)/255;
-					const roundedLuminosity = Math.round(rawLuminosity * 1000) / 1000;
-					rowArray.push(roundedLuminosity);
+					const roundedLuminosity = Math.round(rawLuminosity * 100) / 100;
+
+					pixels.push({
+						x,
+						y,
+						luminosity: roundedLuminosity,
+					});
 				}
-				fileLuminosity.push(rowArray);
 			}
 			output.files[index] = {
 				delay: image.delay,
-				luminosity: fileLuminosity
+				pixels
 			};
-			if (index === images.length - 1) {
+			if (Object.keys(output.files).length === images.length) {
+				console.log(`${Object.keys(output.files).join(',')}`);
+				for(let currentFrameIndex = 1; currentFrameIndex < images.length; currentFrameIndex+=1){
+					const previousFrame = output.files[currentFrameIndex -1];
+					const currentFrame = output.files[currentFrameIndex];
+					console.log(`previous ${currentFrameIndex -1} current ${currentFrameIndex}`);
+					let framePercentage = 0;
+					const xComparatorConst = 100000;
+					currentFrame.pixels = currentFrame.pixels.filter((currentPixel) => {
+						const pixelPercentage = Math.round((currentPixel.x / width)*100);
+						if(pixelPercentage !== framePercentage){
+							framePercentage = pixelPercentage;
+							console.log(`frame ${currentFrameIndex} at ${framePercentage}%`);
+						}
+						const previousPixelIndex = binarySearch(previousFrame.pixels, currentPixel, (a, b) => {
+							return ((a.x * xComparatorConst) + a.y) - ((b.x * xComparatorConst) + a.y);
+						});
+						if(previousPixelIndex < 0){
+							return true;
+						}
+						const previousPixel = previousFrame.pixels[previousPixelIndex];
+						if(!previousPixel){
+							return true;
+						}
+						return currentPixel.luminosity !== previousPixel.luminosity;
+					})
+				}
+				for(let x = 0; x < images.length; x++){
+					console.log(`frame ${x} has ${output.files[x].pixels.length} pixels`);
+				}
 				fs.writeFileSync(path.join(outDir, 'luminosity.json'), JSON.stringify(output));
 			}
 		});
