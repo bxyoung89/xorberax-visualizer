4208fa72073d9fe30cd1e76452a2ddf83e437041
Broderick Young <bxyoung89@gmail.com>
2018-05-03 02:05:10 -0500

    Added more content.

--- js/canvas-renderers.js
+++ js/canvas-renderers.js
@@ -4,6 +4,11 @@ export default {
 		canvasContext.fillStyle = luminosity > 0.5 ? '#FFF' : '#000';
 		canvasContext.fillRect(x * scale + leftPadding, y * scale + topPadding, scale, scale);
 	},
+	basicPaletteLuminosity: (canvasContext, pixel, scale, leftPadding, topPadding, pallete) => {
+		const {x, y, luminosity} = pixel;
+		canvasContext.fillStyle = luminosity > 0.5 ? pallete(1) : pallete(0);
+		canvasContext.fillRect(x * scale + leftPadding, y * scale + topPadding, scale, scale);
+	},
 	paletteLuminosity: (canvasContext, pixel, scale, leftPadding, topPadding, pallete) => {
 		const {x, y, luminosity} = pixel;
 		canvasContext.fillStyle = pallete(luminosity);
