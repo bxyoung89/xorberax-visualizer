568b27d41fff677a5e313fd1738f17d76037afcb
Broderick Young <bxyoung89@gmail.com>
2018-05-03 01:45:53 -0500

    Got all the dropdowns working.

--- js/canvas-renderers.js
+++ js/canvas-renderers.js
@@ -4,7 +4,7 @@ export default {
 		canvasContext.fillStyle = luminosity > 0.5 ? '#FFF' : '#000';
 		canvasContext.fillRect(x * scale + leftPadding, y * scale + topPadding, scale, scale);
 	},
-	palletLuminosity: (canvasContext, pixel, scale, leftPadding, topPadding, pallete) => {
+	paletteLuminosity: (canvasContext, pixel, scale, leftPadding, topPadding, pallete) => {
 		const {x, y, luminosity} = pixel;
 		canvasContext.fillStyle = pallete(luminosity);
 		canvasContext.fillRect(x * scale + leftPadding, y * scale + topPadding, scale, scale);
--- js/main.js
+++ js/main.js
@@ -7,16 +7,51 @@ import styleElementsToPalette from './style-elements-to-palette.js';
 
 const canvasId = 'gif-canvas';
 let currentPalette = palettes.black2Red;
-let currentCanvasRenderer = canvasRenderers.palletLuminosity;
+let currentCanvasRenderer = canvasRenderers.paletteLuminosity;
+let lastGif = undefined;
 styleElementsToPalette(currentPalette);
-
-
 
 const gifRenderer = new GifRenderer(canvasId, loadingGifData, currentPalette, currentCanvasRenderer);
 gifRenderer.runAnimation();
 
-getImage('pimp-hank-with-money.json').then((json) => {
-// getImage('shibe-puppers-eat-lettuce.json').then((json) => {
-	gifRenderer.loadNewSettings(json, currentPalette, currentCanvasRenderer);
+const runLoadingIndicator = () => {
+	gifRenderer.loadNewSettings(loadingGifData, currentPalette, currentCanvasRenderer);
 	gifRenderer.runAnimation();
+};
+
+const reloadEverything = () => {
+	styleElementsToPalette(currentPalette);
+	gifRenderer.loadNewSettings(lastGif, currentPalette, currentCanvasRenderer);
+	gifRenderer.runAnimation();
+};
+
+const onImageChange = (event) => {
+	runLoadingIndicator();
+	getImage(event.target.value).then((json) => {
+		lastGif = json;
+		reloadEverything();
+	});
+};
+
+const onPaletteChange = (event) => {
+	runLoadingIndicator();
+	currentPalette = palettes[event.target.value];
+	reloadEverything();
+};
+
+const onCanvasRendererChange = (event) => {
+	runLoadingIndicator();
+	currentCanvasRenderer = canvasRenderers[event.target.value];
+	reloadEverything();
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('#image-selector').onchange = onImageChange;
+	document.querySelector('#palette-selector').onchange = onPaletteChange;
+	document.querySelector('#canvas-renderer-selector').onchange = onCanvasRendererChange;
+}, false);
+
+getImage('pimp-hank-with-money.json').then((json) => {
+	lastGif = json;
+	reloadEverything();
 });
