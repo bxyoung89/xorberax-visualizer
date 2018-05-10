25db03dcc65acc44c23ed8bffc649ab102549bc7
Broderick Young <bxyoung89@gmail.com>
2018-05-09 23:41:18 -0500

    Added more audio.

--- js/data/audio.js
+++ js/data/audio.js
@@ -9,4 +9,39 @@ export default [
 		name: 'That Equals This',
 		filePath: 'zooloo75/that-equals-this.mp3'
 	},
+	{
+		artist: 'zooloo75',
+		name: 'Another Cookie',
+		filePath: 'zooloo75/another-cookie.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'Burgers, Fries, and Shakes',
+		filePath: 'zooloo75/burgers-fries-and-shakes.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'I\'m Gonna Miss You',
+		filePath: 'zooloo75/im-gonna-miss-you.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'Nu-stalgia',
+		filePath: 'zooloo75/nu-stalgia.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'Scratched Brass',
+		filePath: 'zooloo75/scratched-brass.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'Slow it Down',
+		filePath: 'zooloo75/slow-it-down.mp3'
+	},
+	{
+		artist: 'zooloo75',
+		name: 'Success',
+		filePath: 'zooloo75/success.mp3'
+	},
 ];
